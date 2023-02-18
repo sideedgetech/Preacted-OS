b961f6605ce4b71c531ebfe54869e36d1bb636f0
Side Edge <sideedgetech@gmail.com>
2023-02-18 18:52:50 +0530

    Update purus-twitter.app-config.ts

--- src/data/apps/purus-twitter.app-config.ts
+++ src/data/apps/purus-twitter.app-config.ts
@@ -5,7 +5,7 @@ export const purusTwitterAppConfig = createAppConfig({
   resizable: true,
 
   shouldOpenWindow: false,
-  externalAction: () => window.open('https://twitter.com/puruvjdev', '_blank'),
+  externalAction: () => window.open('https://github.com/sidedgetech', '_blank'),
 
   dockBreaksBefore: true,
 });
